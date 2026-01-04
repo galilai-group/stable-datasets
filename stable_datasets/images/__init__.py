cc8621a5d2644ce46584220453302164cddff669
luizfysouza <luizfysouza@gmail.com>
2026-01-04 14:14:03 -0300

    uc_merced addition

--- stable_datasets/images/__init__.py
+++ stable_datasets/images/__init__.py
@@ -15,8 +15,6 @@ from .cifar100 import CIFAR100
 # from .cifar100 import CIFAR100
 from .cifar100_c import CIFAR100C
 from .clevrer import CLEVRER
-from .dtd import DTD
-from .e_mnist import EMNIST
 
 # from .country211 import Country211
 # from .cub200 import CUB200
@@ -25,6 +23,7 @@ from .dsprites_color import DSpritesColor
 from .dsprites_noise import DSpritesNoise
 from .dsprites_scream import DSpritesScream
 from .dtd import DTD
+from .e_mnist import EMNIST
 from .fashion_mnist import FashionMNIST
 from .flowers102 import Flowers102
 from .k_mnist import KMNIST
@@ -44,6 +43,7 @@ from .not_mnist import NotMNIST
 # from .rock_paper_scissor import RockPaperScissor
 # from .stl10 import STL10
 from .svhn import SVHN
+from .uc_merced import UCMerced
 
 
 # from .tiny_imagenet import TinyImagenet
@@ -71,4 +71,5 @@ __all__ = [
     "NotMNIST",
     "Flowers102",
     "SVHN",
+    "UCMerced",
 ]
