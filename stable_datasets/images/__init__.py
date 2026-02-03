790423eb72ba3ce5900d3ef8b794dc7c70f382e9
Haodong Zhang <128533950+haodongzhang0118@users.noreply.github.com>
2026-02-03 15:44:14 -0500

    [Dataset] Cars3D, Shapes3D, Small_Norb and Food101 (#34)
    * Added .DS_Store into .gitignore
    
    * Added cars3d
    
    * Delete .DS_Store
    
    * Added small norb dataset
    
    * Updated small norb
    
    * Added shapes3d
    
    * Updated __init__.py
    
    * Added food101 and updated cars196/DTD rst file
    
    * Added food101_teaser.png
    
    * Back utils to original
    
    * Run precommit fix
    
    ---------
    
    Co-authored-by: leonleyang <82098977+Leon-Leyang@users.noreply.github.com>
    Co-authored-by: leyang <leyang_hu@brown.edu>

--- stable_datasets/images/__init__.py
+++ stable_datasets/images/__init__.py
@@ -2,6 +2,7 @@
 
 from .arabic_characters import ArabicCharacters
 from .arabic_digits import ArabicDigits
+from .cars3d import CARS3D
 from .cars196 import Cars196
 from .cifar10 import CIFAR10
 from .cifar10_c import CIFAR10C
@@ -16,13 +17,11 @@ from .dtd import DTD
 from .e_mnist import EMNIST
 from .fashion_mnist import FashionMNIST
 from .flowers102 import Flowers102
+from .food101 import Food101
 from .k_mnist import KMNIST
 from .linnaeus5 import Linnaeus5
 from .med_mnist import MedMNIST
 from .not_mnist import NotMNIST
-from .stl10 import STL10
-from .svhn import SVHN
-
 
 # from .awa2 import AWA2
 # from .beans import Beans
@@ -36,6 +35,12 @@ from .svhn import SVHN
 # from .mnist import MNIST
 # from .places365_small import Places365Small
 # from .rock_paper_scissor import RockPaperScissor
+from .shapes3d import Shapes3D
+from .small_norb import SmallNORB
+from .stl10 import STL10
+from .svhn import SVHN
+
+
 # from .tiny_imagenet import TinyImagenet
 # from .tiny_imagenet_c import TinyImagenetC
 
@@ -43,6 +48,7 @@ __all__ = [
     "ArabicCharacters",
     "ArabicDigits",
     "Cars196",
+    "CARS3D",
     "CIFAR10",
     "CIFAR10C",
     "CIFAR100",
@@ -59,6 +65,11 @@ __all__ = [
     "KMNIST",
     "MedMNIST",
     "NotMNIST",
+    "Flowers102",
+    "Food101",
+    "SVHN",
+    "Shapes3D",
+    "SmallNORB",
     "STL10",
     "SVHN",
     "Linnaeus5",
