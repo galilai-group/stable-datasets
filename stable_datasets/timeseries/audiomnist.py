c571583854db294b4bace372eb24a5b5c4b2d98e
leyang <leyang_hu@brown.edu>
2026-05-15 15:23:19 -0400

    Update citation

--- stable_datasets/timeseries/audiomnist.py
+++ stable_datasets/timeseries/audiomnist.py
@@ -27,11 +27,15 @@ class AudioMNIST(BaseDatasetBuilder):
                 filename="AudioMNIST-master.zip",
             ),
         },
-        citation="""@misc{becker2018audiomnist,
-                     title={AudioMNIST: Exploring spoken digits from zero to hero},
-                     author={Soren Becker and Marcin Ackermann and Seong Han and Klaus-Robert Muller and Marco Doeller},
-                     year={2018},
-                     howpublished={GitHub repository}}""",
+        citation="""@article{audiomnist2023,
+                     title = {AudioMNIST: Exploring Explainable Artificial Intelligence for audio analysis on a simple benchmark},
+                     journal = {Journal of the Franklin Institute},
+                     year = {2023},
+                     issn = {0016-0032},
+                     doi = {https://doi.org/10.1016/j.jfranklin.2023.11.038},
+                     url = {https://www.sciencedirect.com/science/article/pii/S0016003223007536},
+                     author = {Sören Becker and Johanna Vielhaben and Marcel Ackermann and Klaus-Robert Müller and Sebastian Lapuschkin and Wojciech Samek},
+                     keywords = {Deep learning, Neural networks, Interpretability, Explainable artificial intelligence, Audio classification, Speech recognition}}""",
     )
 
     def _info(self):
